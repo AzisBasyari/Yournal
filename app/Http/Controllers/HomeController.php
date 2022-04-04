0ff55680b0cd24666ca13e1d6a6c1c3acc3f2555
AzisBasyari <azisbasyari3010@gmail.com>
2022-04-04 18:36:23 +0700

    Add Search Feature

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,16 +13,18 @@ class HomeController extends Controller
     public function main()
     {
 
-        $catatan = Catatan::whereUserId(Auth::id())->orderBy('created_at', 'desc');
+        $catatan = Catatan::whereUserId(Auth::id());
         $logs = DB::table('logs')->where('logs.user_id', '=', Auth::id())->select('*', 'logs.updated_at as logs_updated_at')->orderBy('logs.created_at', 'desc')->paginate(5, ['*'], 'log');
 
         return view('main', [
             "title" => "YOURNAL",
             "catatans" => $catatan->paginate(5, ['*'], 'home'),
-            "posts" => $catatan->paginate(10, ['*'], 'manage'),
+            "posts" => $catatan->order(request(['filter', 'order']))->search(request(['search']))->paginate(10, ['*'], 'manage'),
             "logs" => $logs,
             "no" => 1
         ]);
+
+        
     }
 
     public function catatan(Catatan $catatan){
--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -20,11 +20,72 @@
 
 <section class="mt-5">
     <h1 class="fs-36">Kelola catatan</h1>
+    <form action="/main" method="get">
+        @csrf
+        <div class="row mt-3">
+            <div class="col-lg-2">
+                <label for="filter" class="form-label">Urut Berdasarkan</label>
+                <select class="form-select" aria-label="filter" name="filter" id="filter">
+                    <option value="">
+                        @if (request('filter') == 'tanggal_perjalanan')
+                            Tanggal
+                        @elseif (request('filter') == 'jam_perjalanan')
+                            Jam
+                        @elseif (request('filter') == 'nama_tempat')
+                            Nama Tempat
+                        @elseif (request('filter') == 'alamat')
+                            Alamat
+                        @elseif (request('filter') == 'suhu_tubuh')
+                            Suhu Tubuh
+                        @else
+                            Pilih
+                        @endif
+                    </option>
+                    <option value="" disabled>---------------------</option>
+                    <option value="tanggal_perjalanan">Tanggal</option>
+                    <option value="jam_perjalanan">Jam</option>
+                    <option value="nama_tempat">Nama Tempat</option>
+                    <option value="alamat">Alamat</option>
+                    <option value="suhu_tubuh">Suhu Tubuh</option>
+                </select>
+            </div>
+            <div class="col-lg-3">
+                <label for="order" class="form-label">Urutan</label>
+                <select class="form-select" aria-label="order" name="order" id="order">
+                    <option value="">
+                        @if (request('order') == 'desc')
+                            Berurutan Turun
+                        @elseif (request('order') == 'asc')
+                            Berurutan Naik
+                        @else
+                            Pilih
+                        @endif
+                    </option>
+                    <option value="" disabled>---------------------</option>
+                    <option value="desc">Berurutan Turun</option>
+                    <option value="asc">Berurutan Naik</option>
+                </select>
+            </div>
+            <div class="col-lg-5">
+                <label for="search" class="form-label">Cari Berdasarkan Tanggal<span class="text-danger">*</span>, Jam, Tempat, Alamat, dan Suhu</label>
+                <input type="text" class="form-control" id="search" name="search" placeholder="Cari" value="{{ request('search') }}">
+            </div>
+            <div class="col-lg-2 mt-4 pt-1 mb-2">
+                <button type="submit" class="btn btn-primary rounded-pill px-4">
+                    <img src="https://img.icons8.com/material-rounded/24/ffffff/search.png">
+                </button>
+                <a href="/main" role="button" class="btn btn-primary rounded-pill px-4">
+                    <img src="https://img.icons8.com/material-rounded/24/ffffff/reset.png">
+                </a>
+            </div>
+        </div>
+    </form>
+    <span class="text-danger">*Format pencarian berdasarkan tanggal adalah menggunakan angka, contoh: 2022-12-31 (Tahun-Bulan-Tanggal)</span>
 </section>
 
-<section class="mt-5">
+<section class="mt-3">
     <table class="table table-bordered border-white">
-        <thead class="bg-primary text-white">
+        <thead class="bg-primary text-white text-center">
             <tr>
                 <th scope="col">No.</th>
                 <th scope="col">Tanggal</th>
@@ -39,18 +100,18 @@
             @foreach ($posts as $catatan)
             
                 <tr>
-                    <th>{{ $no++ }}</th>
-                    <td>{{ date('d F Y', strtotime($catatan->tanggal_perjalanan)) }}</td>
-                    <td>{{ date('H : i', strtotime($catatan->jam_perjalanan)) }}</td>
+                    <th class="text-center">{{ $no++ }}</th>
+                    <td class="text-center">{{ date('d F Y', strtotime($catatan->tanggal_perjalanan)) }}</td>
+                    <td class="text-center">{{ date('H : i', strtotime($catatan->jam_perjalanan)) }}</td>
                     <td>{{ $catatan->nama_tempat }}</td>
                     <td>{{ $catatan->alamat }}</td>
-                    <td>{{ $catatan->suhu_tubuh }}&#176;</td>
-                    <td>
+                    <td class="text-center">{{ $catatan->suhu_tubuh }}&#176;</td>
+                    <td class="text-center">
                         <a href="/catatan-{{ $catatan->id }}" role="button">
                             <img src="https://img.icons8.com/material-rounded/24/80B5DF/eye.png">
                         </a>
                     </td>
-                    <td>
+                    <td class="text-center">
                         <button type="button" class="btn p-0 m-0" data-bs-toggle="modal" data-bs-target="#edit" 
                         data-bs-id="{{ $catatan->id }}"
                         data-bs-tempat="{{ $catatan->nama_tempat }}"
@@ -62,7 +123,7 @@
                             <img src="https://img.icons8.com/material-rounded/24/80B5DF/edit.png">
                         </button>
                     </td>
-                    <td>
+                    <td class="text-center">
                         <form action="/manage-catatan/delete/{{ $catatan->id }}" method="POST">
                             {{-- @method('delete') --}}
                             @csrf
@@ -137,40 +198,40 @@
 
 <script>
     var exampleModal = document.getElementById('edit')
-exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var id = button.getAttribute('data-bs-id')
-  var tanggal = button.getAttribute('data-bs-tanggal')
-  var jam = button.getAttribute('data-bs-jam')
-  var tempat = button.getAttribute('data-bs-tempat')
-  var alamat = button.getAttribute('data-bs-alamat')
-  var suhu = button.getAttribute('data-bs-suhu')
-  var deskripsi = button.getAttribute('data-bs-deskripsi')
-//   var id = button.getAttribute('data-bs-id')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-    var id_input = exampleModal.querySelector("#formEdit");
-    var tanggal_input = exampleModal.querySelector("#tanggal");
-    var jam_input = exampleModal.querySelector("#jam");
-    var tempat_input = exampleModal.querySelector("#tempat");
-    var alamat_input = exampleModal.querySelector("#alamat");
-    var suhu_input = exampleModal.querySelector("#suhu");
-    var deskripsi_trix_input = exampleModal.querySelector("#deskripsi-trix");
-    var deskripsi_input = exampleModal.querySelector("#deskripsi");
+    exampleModal.addEventListener('show.bs.modal', function (event) {
+        // Button that triggered the modal
+        var button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        var id = button.getAttribute('data-bs-id')
+        var tanggal = button.getAttribute('data-bs-tanggal')
+        var jam = button.getAttribute('data-bs-jam')
+        var tempat = button.getAttribute('data-bs-tempat')
+        var alamat = button.getAttribute('data-bs-alamat')
+        var suhu = button.getAttribute('data-bs-suhu')
+        var deskripsi = button.getAttribute('data-bs-deskripsi')
+        //   var id = button.getAttribute('data-bs-id')
+        // If necessary, you could initiate an AJAX request here
+        // and then do the updating in a callback.
+        //
+        // Update the modal's content.
+        var id_input = exampleModal.querySelector("#formEdit");
+        var tanggal_input = exampleModal.querySelector("#tanggal");
+        var jam_input = exampleModal.querySelector("#jam");
+        var tempat_input = exampleModal.querySelector("#tempat");
+        var alamat_input = exampleModal.querySelector("#alamat");
+        var suhu_input = exampleModal.querySelector("#suhu");
+        var deskripsi_trix_input = exampleModal.querySelector("#deskripsi-trix");
+        var deskripsi_input = exampleModal.querySelector("#deskripsi");
 
-    var url_edit = `/manage-catatan/edit/${id}`;
+        var url_edit = `/manage-catatan/edit/${id}`;
 
-    id_input.setAttribute("action", url_edit);
-    tanggal_input.value = tanggal
-    jam_input.value = jam
-    tempat_input.value = tempat
-    alamat_input.value = alamat
-    suhu_input.value = suhu
-    deskripsi_trix_input.value = deskripsi
-    deskripsi_input.value = deskripsi
-});
+        id_input.setAttribute("action", url_edit);
+        tanggal_input.value = tanggal
+        jam_input.value = jam
+        tempat_input.value = tempat
+        alamat_input.value = alamat
+        suhu_input.value = suhu
+        deskripsi_trix_input.value = deskripsi
+        deskripsi_input.value = deskripsi
+    });
 </script>
